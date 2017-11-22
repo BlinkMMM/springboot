a721f31d70498047a73b0461cf87461269254a91
hzheng2 <hzheng2@amarsoft.com>
2017-11-22 18:19:52 +0800

    feat:use mybatis-generator

--- src/main/java/com/demo/springboot/Application.java
+++ src/main/java/com/demo/springboot/Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by hzheng2 on 17-11-20.
- * 这里使用mybatis作为持久层，但是不适用xml那种方式
- * 而是使用 @Select("select * from book") 注解方式
+ * 这里使用mybatis作为持久层
+ * 使用mybatis-generator插件，自动生成model，dao和mapper
+ *
+ * 最后使用 ：mvn mybatis-generator:generate这个命令即可
  */
 @SpringBootApplication
 @MapperScan("com.demo.springboot.dao")
