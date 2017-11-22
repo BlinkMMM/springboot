88cece06c830b11c15876c515b7d1fcde0a3c4f4
hzheng2 <hzheng2@amarsoft.com>
2017-11-22 15:06:20 +0800

    feat:add unitTest for springBoot

--- src/test/java/test/Test01.java
+++ src/test/java/test/Test01.java
@@ -1,7 +1,32 @@
 package test;
 
+import com.demo.springboot.Application;
+import com.demo.springboot.entity.Book;
+import com.demo.springboot.service.BookService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
 /**
  * Created by hzheng2 on 17-11-22.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
 public class Test01 {
+    @Autowired
+    BookService bookService;
+
+    @Test
+    public void testFindAllBook() {
+        List<Book> bookList = bookService.findAllBooks();
+        Assert.assertNotNull(bookList);
+        Assert.assertEquals(bookList.size(),4);
+    }
 }
