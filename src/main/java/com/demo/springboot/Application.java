76de29fae07311d33f546f439412347eb7dc2085
hzheng2 <hzheng2@amarsoft.com>
2017-11-22 17:38:29 +0800

    feat:add description for develop

--- src/main/java/com/demo/springboot/Application.java
+++ src/main/java/com/demo/springboot/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by hzheng2 on 17-11-20.
+ * 这里使用mybatis作为持久层，但是不适用xml那种方式
+ * 而是使用 @Select("select * from book") 注解方式
  */
 @SpringBootApplication
 @MapperScan("com.demo.springboot.dao")
