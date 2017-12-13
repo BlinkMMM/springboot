5739601de812b18d336f57f00d5c721f17761a29
zhenghan <hzheng2@amarsoft.com>
2017-12-13 11:24:10 +0800

    feat:add annotation

--- src/main/java/com/demo/springboot/Application.java
+++ src/main/java/com/demo/springboot/Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  * Created by hzheng2 on 17-11-20.
  * 这里使用mybatis作为持久层，但是不适用xml那种方式
  * 而是使用 @Select("select * from book") 注解方式
+ * 可以打war包
  */
 @SpringBootApplication
 @MapperScan("com.demo.springboot.dao")
